43161584bea39781453030898f99a285c71f82f4
Valentin-Semur <valentin.semur@gmail.com>
2024-09-05 15:53:06 +0200

    add LeafNode and tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -26,3 +26,27 @@ class HTMLNode():
 
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
+
+    
+class LeafNode(HTMLNode):
+    def __init__(
+            self,
+            tag: Optional[str] = None,
+            value: Optional[str] = None,
+            props: Optional[Dict[str, str]] = None
+    ) -> None:
+        super().__init__(
+            tag=tag,
+            value=value,
+            props=props
+        )
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("Invalid HTML: no value")
+        if self.tag is None:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
